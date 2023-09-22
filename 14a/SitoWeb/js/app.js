28083820df2e0f8df1fa22c7aeed7a936757a342
AdrianSanta1 <141043080+AdrianSanta1@users.noreply.github.com>
2023-09-21 22:27:05 -0500

    Add files via upload

--- 14a/SitoWeb/js/app.js
+++ 14a/SitoWeb/js/app.js
@@ -7,24 +7,75 @@ const btnEnviar = document.getElementById('btnSend');
 const form = document.getElementById('form');
 
 //objeto validacion
+
 const formValid = {
-    nombres: false, 
+    nombres: false,
     apellidos: false,
     mail: false,
     celphone: false,
     politica: false
-
-}
-btnEnviar.addEventListener("click",(e)=>{
+ }
+ btnEnviar.addEventListener("click",(e)=>{
     e.preventDefault();
-
-});
-
-form.addEventListener("change",(e)=>{
+    if(validInputsForm(formValid)===-1){
+        alert("Formulario Invalido");
+    }else{
+        alert("Formulario Enviado");
+    }
+ });
+ const validInputsForm =(objeto)=>{
+    const values = Object.values(objeto);
+    let response = values.findIndex(e=>e===false);
+    return response;
+ }
+ 
+ form.addEventListener("change",(e) =>{
     const inputId = e.target.id;
     console.log(inputId);
-});
+    const valueInput = e.target.value;
+    console.log(valueInput);
+    const classInput = e.target.classList;
+    console.log(classInput);
+    //funciones para agregar estilos y quitar estilos
+    const isValidClass =  () => {
+       classInput.add("is-valid");
+       classInput.remove("is-invalid"); 
+    }
+    const isInvalidClass = () => {
+       classInput.remove("is-valid");
+       classInput.add("is-invalid"); 
+    }
+    switch (inputId)   {
+       case "names":
+          const nombresRx =
+        /^([a-zA-ZÀ-ÖØ-öø-ÿ]{3,25})([\s]?)([a-zA-ZÀ-ÖØ-öø-ÿ]{0,25})$/g;
+        formValid.nombres= valueInput.match(nombresRx) ? true : false;
+        formValid.nombres ? isValidClass() : isInvalidClass();
+        console.log(Object.values(formValid));
 
+        break;
+        case "lastNames":
+            const lastNamesRx =
+        /^([a-zA-ZÀ-ÖØ-öø-ÿ]{3,25})([\s]?)([a-zA-ZÀ-ÖØ-öø-ÿ]{0,25})$/g;
+        formValid.lastNames= valueInput.match(lastNamesRx) ? true : false;
+        formValid.lastNames ? isValidClass() : isInvalidClass();
+        console.log(Object.values(formValid));
+
+        break;
+        case "mail":
+            const mailRx = /^([\w.]+[^#$%&\/()='"!?¡]\w*-*)([@])(\w)+(\.[a-z]{2,3})$/g;
+            formValid.mail= valueInput.match(mailRx) ? true : false;
+            formValid.mail ? isValidClass() : isInvalidClass();
+            console.log(Object.values(formValid));
+        break;
+        case "celphone":
+            const celphoneRx = /^[0-9]{10}$/;
+            formValid.celphone= valueInput.match(celphoneRx) ? true : false;
+            formValid.celphone ? isValidClass() : isInvalidClass();
+            console.log(Object.values(formValid));
+        break;
+    }
+ } ) ;
 
 
 //const fragment = document.createDocumentFragment();
